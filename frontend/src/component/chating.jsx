bfad6bd8e175f24abe356813ddb07d0e62fdf0f0
Prasad Ramdas Hiwarkhede <143614636+HiwarkhedePrasad@users.noreply.github.com>
2025-03-03 00:34:39 +0530

    Update chating.jsx

--- frontend/src/component/chating.jsx
+++ frontend/src/component/chating.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://chatcript.onrender.com");
 
 const RoomPage = () => {
   const [showForm, setShowForm] = useState(false);
